0f242e139bda3219bc0a6eca1a4df0cc0f8a6f6d
TRaven34 <116375899+TRaven34@users.noreply.github.com>
2022-10-21 14:08:59 -0500

    Add files via upload

---
+++ 6.Analizador Semantico/AnalizadorSemanticoC#VisualStudio/Error.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analizador_Lexico__Traductor_
+{
+    internal class Error
+    {
+        //ATRIBUTOS//
+        private string Cadena;
+        private string Exception;
+
+        //CONSTRUCTOR//
+        public Error(string NewCadena, string NewException)
+        {
+            Cadena = NewCadena;
+            Exception = NewException;
+        }
+
+        //SETTERS//
+        public void SetCadena(string NewCadena) { Cadena = NewCadena; }
+        public void SetException(string NewException) { Exception = NewException; }
+
+        //GETTERS//
+        public string Instruccion {  get { return Cadena; } }
+        public string Excepcion { get { return Exception; } } 
+    }
+}
